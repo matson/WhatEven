4a11a1299829b344bab47d274304e01ac7891dcd
Madison <madison.k.adams@gmail.com>
2024-02-20 12:23:02 -0300

    packages and register

--- WhatEven/RegisterViewController.swift
+++ WhatEven/RegisterViewController.swift
@@ -7,7 +7,32 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController{
     //comment here to commit test
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func register(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().createUser(withEmail: email, password: password) {
+                authResult, error in
+                if let e = error {
+                    print(error)
+                    //take care of this later
+                }else{
+                    //go to homepage.
+                    
+                }
+                
+            }
+        }
+        
+        
+    }
+    
 }
